dce8106b1e6bfb5f01cdaa5e01e28211944970c4
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-03-18 00:16:39 +0100

    disabled the decrypt, exception in decrypt url

--- src/main/java/net/alepuzio/authsys/domain/user/persistence/ConnectionFactory.java
+++ src/main/java/net/alepuzio/authsys/domain/user/persistence/ConnectionFactory.java
@@ -5,11 +5,13 @@ import java.sql.DriverManager;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import net.alepuzio.authsys.config.MariaDBConfig;
 import net.alepuzio.authsys.crypto.TrippleDes;
 import net.alepuzio.authsys.crypto.exception.MyException;
 
+@Component
 public class ConnectionFactory {
 
 	@Autowired
@@ -21,12 +23,13 @@ public class ConnectionFactory {
 		Connection result = null;
 		try {
 			Class.forName(this.mariaDBConfig.getDriver());
-			result = DriverManager.getConnection(new TrippleDes().decrypt(this.mariaDBConfig.getUrl()),
-					new TrippleDes().decrypt(this.mariaDBConfig.getUsername()),
-					new TrippleDes().decrypt(this.mariaDBConfig.getPassword()));
-			logger.info("Connected database successfully...");
+			/*TODO correggere errore javax.crypto.IllegalBlockSizeException: Input length must be multiple of 16 when decrypting with padded cipher*/
+			result = DriverManager.getConnection(/* new TrippleDes().decrypt( */this.mariaDBConfig.getUrl()/* ) */,
+					/* new TrippleDes().decrypt( */this.mariaDBConfig.getUsername()/* ) */,
+					/* new TrippleDes().decrypt( */this.mariaDBConfig.getPassword())/* ) */;
+			logger.debug("Connected database successfully...");
 		} catch (Exception e) {
-			new MyException(e,logger).error().exception();
+			new MyException(e, logger).error().exception();
 		}
 		return result;
 	}
