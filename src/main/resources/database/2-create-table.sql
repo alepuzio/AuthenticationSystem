d1255c1096edfd2424cd74f661d8d4bad7a310f4
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-04-12 11:38:20 +0200

    created specified tabels for spring jpa

--- src/main/resources/database/2-create-table.sql
+++ src/main/resources/database/2-create-table.sql
@@ -12,5 +12,35 @@ COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 ;
 
+--spring JPA
+CREATE TABLE `user_anagraphic` (
+	`name` VARCHAR(50)  NOT NULL  DEFAULT '' COLLATE 'latin1_swedish_ci' ,
+	`surname` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+	`vatin` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+	    primary key(vatin)
+
+)
+COMMENT='Table of the anagraphic data of the recorded users'
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+CREATE TABLE `user_security` (
+	`vatin` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+	`username` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+	`password` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+	    primary key(vatin),
+	    CONSTRAINT `fk_vatin`
+	    FOREIGN KEY (vatin) REFERENCES user_anagraphic (vatin)
+	    ON DELETE CASCADE
+		ON UPDATE RESTRICT
+)
+COMMENT='Table of the security data of the recorded users'
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+
+
+
 COMMIT;
+
 
--- src/main/resources/database/3-create-dummy-data.sql
+++ src/main/resources/database/3-create-dummy-data.sql
@@ -9,4 +9,9 @@
 INSERT INTO `user` (`name`, `surname`, `vatin`, `username`, `password`) VALUES
 	('X3uX/Zv+ic759LEjXLp8Lg==', '8LDGA+D4NISJJSpQl9fUJg==', '55rRxPs69z0=', 'wQNoUUT6TjI=', 'UBWYNFPNYYY=');
 
+
+--spring JPA
+INSERT INTO `user_anagraphic` (`name`, `surname`, `vatin`) VALUES ('X3uX/Zv+ic759LEjXLp8Lg==', '8LDGA+D4NISJJSpQl9fUJg==', '55rRxPs69z0=');
+INSERT INTO `user_security` (`vatin`, `username`, `password`) VALUES ('55rRxPs69z0=', 'wQNoUUT6TjI=', 'UBWYNFPNYYY=');
+
 commit;
